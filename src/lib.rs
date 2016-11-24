5af77b17a2e7670216ae367aefb884351816d876
bluss <bluss@users.noreply.github.com>
2016-11-24 01:43:39 +0100

    Add iterator adaptor .cumsum()

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,18 @@
 extern crate num_traits;
 
 mod linspace;
+mod ext;
 
-pub use linspace::{linspace, Linspace};
+pub use linspace::linspace;
+#[doc(no_inline)]
+pub use structs::Linspace;
+pub use ext::ItertoolsNum;
+
+/// The concrete iterator structs.
+pub mod structs {
+    pub use ext::Cumsum;
+    pub use linspace::Linspace;
+}
 
 #[cfg(test)]
 mod tests {
